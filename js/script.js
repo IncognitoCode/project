f1fc0188b513733c06a49b30d0e3426ff725aca6
Incognito <amnkcinkognitocode@gmail.com>
2016-03-29 00:33:38 +0300

    next image, animation, check errors

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-function hideBig(){
+function hideBig() {
     var big = document.getElementById("big");
     big.style.display = "none";
     var shadow = document.getElementById('shadow');
     shadow.style.display = 'none';
 }
-function showBig(s){
+function showBig(s) {
     var big = document.getElementById('big');
     big.style.display = 'block';
     var shadow = document.getElementById('shadow');
     shadow.style.display = 'block';
-    var img = "../images/"+s+".jpg";
-    big.style.background = "url('"+img+"')";
+    var img = "images/"+s+".jpg";
+    big.style.background = "url("+img+")";
+}
+function nextImage() {
+    var big = document.getElementById("big");
+    var bg = big.style.backgroundImage;
+    var next = Number(bg[12])+1;
+    if(next > 9)
+        showBig(1);
+    else
+        showBig(next);
 }
