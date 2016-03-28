15b7ee7cb33f74864c362fdda16de26073a6b88d
Andrian <heroandrik@gmail.com>
2016-03-28 21:59:42 +0300

    finish showBig

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-function showBig() {
+function showBig(s) {
     var big = document.getElementById('big');
     big.style.display = 'block';
-
+    var shadow = document.getElementById('shadow');
+    shadow.style.display = 'block';
+    var img = "../images/"+s+".jpg";
+    big.style.background = "url('"+img+"')";
 }
