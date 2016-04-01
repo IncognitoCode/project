2c7cba4a2ef3c8b01b0a6f75a555629224b5dd3d
Incognito <amnkcinkognitocode@gmail.com>
2016-04-01 21:19:41 +0300

    restore

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Devices</title>
+<title>Connected Devices</title>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="css/style.css"/>
 <script src="js/jquery.js"></script>
